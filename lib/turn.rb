e0c8e782a7742c537f61d8fcb075ebddc929f9b5
Alan Cooke <atc4@rice.edu>
2021-01-06 18:47:28 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,4 +1,18 @@
-def display_board
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(input)
+  input = (input.to_i - 1)
+end
+
+def move(array, index, value = "X")
+  array[index] = value
+end
 
 def valid_move?(board, index)
  if index.between?(1,9)
@@ -13,5 +27,3 @@ end
 def position_taken?(board, index)
   board[index] != " "
 end
-
-def move
