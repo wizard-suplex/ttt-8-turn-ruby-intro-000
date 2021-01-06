745f092c5a452e0f913b95c5d54ea39141e739eb
Alan Cooke <atc4@rice.edu>
2021-01-06 18:36:25 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,17 @@
+def display_board
+
+def valid_move?(board, index)
+ if index.between?(1,9)
+   if !position_taken?(board, index)
+     true
+   end
+ end
+
+ index.between?(0,8) && !position_taken?(board, index)
+end
+
+def position_taken?(board, index)
+  board[index] != " "
+end
+
+def move
